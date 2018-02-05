2d8fbfb2f993073ea18593f5070b2a8cec8d0398
Mucalytic <mucalytic@hotmail.com>
2018-02-05 08:41:34 +0000

    Update chess-player.ts
    Changed speech rate.

--- chess-player.ts
+++ chess-player.ts
@@ -1,7 +1,9 @@
-let item = Array.from(document.getElementsByClassName("clock"))[0];
-item.addEventListener("DOMSubtreeModified", function () {
-    var hs = item.innerHTML.split(":").map(s => parseInt(s));
+var item = document.getElementsByClassName("clock")[0];
+item.addEventListener("DOMSubtreeModified", () => {
+    var hs = item.innerHTML.split(":").map(s => parseFloat(s));
     if (hs[0] !== 1 && hs[1] % 5 === 0) {
-        window.speechSynthesis.speak(new SpeechSynthesisUtterance(hs[1] + " seconds left"));
+        let utterance = new SpeechSynthesisUtterance(hs[1] + " seconds left");
+        utterance.rate = 1.8;
+        window.speechSynthesis.speak(utterance);
     }
 });
