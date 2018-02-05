af83a9e350230a925be30be0deb0192d3a29bd96
Mucalytic <mucalytic@hotmail.com>
2018-02-05 08:41:02 +0000

    Update chess-player.js
    Changed speech rate.

--- chess-player.js
+++ chess-player.js
@@ -1,7 +1,9 @@
-var item = Array.from(document.getElementsByClassName("clock"))[0];
+var item = document.getElementsByClassName("clock")[0];
 item.addEventListener("DOMSubtreeModified", function () {
-    var hs = item.innerHTML.split(":").map(function (s) { return parseInt(s); });
+    var hs = item.innerHTML.split(":").map(function (s) { return parseFloat(s); });
     if (hs[0] !== 1 && hs[1] % 5 === 0) {
-        window.speechSynthesis.speak(new SpeechSynthesisUtterance(hs[1] + " seconds left"));
+        var utterance = new SpeechSynthesisUtterance(hs[1] + " seconds left");
+        utterance.rate = 1.8;
+        window.speechSynthesis.speak(utterance);
     }
 });
