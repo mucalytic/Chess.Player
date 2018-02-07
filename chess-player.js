e049ea891cba446afdaacf8686d1cbaf0ca97ee3
Aaron <mucalytic@hotmail.com>
2018-02-07 07:49:58 +0000

    Ironed out some bugs and experimented with an alternate approach

--- chess-player.js
+++ chess-player.js
@@ -1,27 +1,28 @@
 var CountdownHelper = (function () {
     function CountdownHelper() {
-        var elem = document.getElementsByClassName("clock")[0];
-        if (elem instanceof HTMLElement) {
-            this.clock = elem;
-        }
+        this.clock = CountdownHelper.getClock();
     }
     CountdownHelper.prototype.start = function () {
-        var _this = this;
-        this.clock.addEventListener("DOMSubtreeModified", function () { return _this.utter(); });
+        this.clock.addEventListener("DOMSubtreeModified", this.utter);
     };
     CountdownHelper.prototype.stop = function () {
-        var _this = this;
-        this.clock.removeEventListener("DOMSubtreeModified", function () { return _this.utter(); });
+        this.clock.removeEventListener("DOMSubtreeModified", this.utter);
     };
     CountdownHelper.prototype.utter = function () {
-        var html = this.clock.innerHTML;
-        var hs = html.split(":").map(function (s) { return parseFloat(s); });
+        var clock = CountdownHelper.getClock();
+        var hs = clock.innerHTML.split(":").map(function (s) { return parseFloat(s); });
         if (hs[0] !== 1 && hs[1] % 5 === 0) {
             var utterance = new SpeechSynthesisUtterance(hs[1] + " seconds left");
             utterance.rate = 1.8;
             window.speechSynthesis.speak(utterance);
         }
-        console.log(JSON.parse(JSON.stringify(hs)));
+        console.log(JSON.stringify(hs));
+    };
+    CountdownHelper.getClock = function () {
+        var clock = document.getElementsByClassName("clock")[0];
+        if (clock instanceof HTMLElement) {
+            return clock;
+        }
     };
     return CountdownHelper;
 }());
@@ -31,22 +32,45 @@ var DomModifier = (function () {
     }
     DomModifier.prototype.addStartAiButton = function () {
         var _this = this;
-        var btnNg = document.getElementsByClassName("btns-container")[0];
-        if (btnNg instanceof HTMLElement) {
-            btnNg.style.cssFloat = "right";
-            var btnAi = btnNg.cloneNode(true);
-            if (btnAi instanceof HTMLElement) {
-                btnAi.style.cssFloat = "left";
-                btnAi.style.marginRight = "12px";
-                var anchor = btnAi.firstChild;
-                if (anchor.nodeName === "A") {
-                    if (anchor instanceof HTMLElement) {
-                        anchor.innerText = "Start AI";
-                        anchor.classList.remove("new-game-btn");
-                        anchor.addEventListener("click", function () { return _this.countdownHelper.start(); });
+        var btnNewGame = document.getElementsByClassName("btns-container")[0];
+        if (btnNewGame instanceof HTMLElement) {
+            btnNewGame.style.cssFloat = "right";
+            var btnOn_1 = btnNewGame.cloneNode(true);
+            if (btnOn_1 instanceof HTMLElement) {
+                btnOn_1.style.cssFloat = "left";
+                btnOn_1.style.marginRight = "12px";
+                var anchorOn = btnOn_1.firstChild;
+                if (anchorOn.nodeName === "A") {
+                    if (anchorOn instanceof HTMLElement) {
+                        anchorOn.innerText = "Start AI";
+                        anchorOn.classList.remove("new-game-btn");
                     }
                 }
-                btnNg.parentNode.appendChild(btnAi);
+                var btnOff_1 = btnOn_1.cloneNode(true);
+                if (btnOff_1 instanceof HTMLElement) {
+                    btnOff_1.style.display = "none";
+                    var anchorOff = btnOff_1.firstChild;
+                    if (anchorOff.nodeName === "A") {
+                        if (anchorOff instanceof HTMLElement) {
+                            anchorOff.innerText = "Stop AI";
+                            anchorOff.style.color = "#b4b4b3";
+                            anchorOff.style.borderBottom = "#272422";
+                            anchorOff.style.backgroundColor = "#272422";
+                            anchorOff.addEventListener("click", function () {
+                                btnOn_1.style.display = "block";
+                                btnOff_1.style.display = "none";
+                                _this.countdownHelper.stop();
+                            });
+                        }
+                    }
+                    anchorOn.addEventListener("click", function () {
+                        btnOff_1.style.display = "block";
+                        btnOn_1.style.display = "none";
+                        _this.countdownHelper.start();
+                    });
+                }
+                btnNewGame.parentNode.appendChild(btnOn_1);
+                btnNewGame.parentNode.appendChild(btnOff_1);
             }
         }
         return this;
@@ -54,10 +78,11 @@ var DomModifier = (function () {
     DomModifier.prototype.rightAlignStartButton = function () {
         var head = document.getElementsByTagName("head")[0];
         if (head instanceof HTMLElement) {
+            var text = document.createTextNode(".btns-container { float: right; }");
             var style = document.createElement("style");
             if (style instanceof HTMLElement) {
                 style.type = "text/css";
-                style.appendChild(document.createTextNode(".btns-container { float: right; }"));
+                style.appendChild(text);
             }
             head.appendChild(style);
         }
