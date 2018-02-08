84918972f23de31e6f86bf18cbc13dfc337a76d7
Mucalytic <mucalytic@hotmail.com>
2018-02-08 14:07:55 +0000

    Update simple-test.js
    Using triple equals

--- simple-test.js
+++ simple-test.js
@@ -8,7 +8,7 @@ changes
         var c = {
             player: a.playerMap[n.attributes["data-piece"].value.charAt(0)],
             piece: a.pieceMap[n.attributes["data-piece"].value.charAt(1)],
-            moved: o.mutation.addedNodes.length == 0 ? "from" : "to",
+            moved: o.mutation.addedNodes.length === 0 ? "from" : "to",
             square: o.mutation.target.attributes["data-square"].value
         };
         var group = a.groups.filter(x => x["group"] === o.datetime);
