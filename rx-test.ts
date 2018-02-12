8089352186c8d59c36bb71255d3a164dae756a7c
Mucalytic <mucalytic@hotmail.com>
2018-02-12 13:28:08 +0000

    Create rx-test.ts
    Working out how to use Rx.JS with MutationObserver.

---
+++ rx-test.ts
@@ -0,0 +1,63 @@
+/// <reference path="./node_modules/rx/ts/rx.d.ts" />
+
+class DomManipulator {
+    src: string = "https://rawgit.com/Reactive-Extensions/RxJS/master/dist/rx.all.min.js";
+
+    addScriptTags(src: string): void {
+        const scripts: NodeListOf<HTMLScriptElement> = document.getElementsByTagName("script");
+        for (let i = 0; i < scripts.length; i++) {
+            if (scripts[i].src === src) {
+                return;
+            }
+        }
+        const script = document.createElement("script");
+        document.body.appendChild(script);
+        script.src = src;
+    }
+
+    constructor() {
+        this.addScriptTags(this.src);
+    }
+}
+
+class DomWatcher {
+    observer: MutationObserver;
+    subscription: Rx.IDisposable;
+    subject: Rx.Subject<MutationRecord>;
+    init: MutationObserverInit = {
+        characterDataOldValue: true,
+        attributeOldValue: true,
+        characterData: true,
+        attributes: true,
+        childList: true,
+        subtree: true
+    };
+
+    createDocumentBodyObserverSubscription(): void {
+        this.subject = new Rx.Subject();
+        this.observer = new MutationObserver(mrs => {
+            mrs.forEach(mr => this.subject.onNext(mr));
+        });
+        this.subscription = this.subject
+            .subscribe(
+                mr => console.log("Rx: Mutation: %o", mr),
+                ex => {
+                    console.log("Rx: Exception: %o", ex);
+                    this.observer.disconnect();
+                },
+                () => {
+                    console.log("Rx: Completed");
+                    this.observer.disconnect();
+                });
+    }
+
+    constructor() {
+        this.createDocumentBodyObserverSubscription();
+        this.observer.observe(document.body, this.init);
+    }
+}
+
+var manipulator = new DomManipulator();
+
+//This code works with http://example.com/
+//var watcher = new DomWatcher();
