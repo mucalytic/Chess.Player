2893b7b2dc0178eaa822843fd951e56eede42419
Mucalytic <mucalytic@hotmail.com>
2018-03-23 17:10:30 +0000

    Update green.ts

--- src/player/green.ts
+++ src/player/green.ts
@@ -4,9 +4,14 @@ import {Player} from "../player"
 export class Green extends Player {
     name: string = "Green";
     turn: number = 4;
+    
+    pivot(): [number, number] {
+        return [13 - this.piece.square.y,
+                     this.piece.square.x];
+    }
 
     rotate(vector: Vector, radius: number): [number, number] {
-        return [this.piece.square.n - vector.x1(radius),
-                this.piece.square.m + vector.y1(radius)];
+        return [this.piece.square.x - vector.x1(radius),
+                this.piece.square.y + vector.y1(radius)];
     }
 }
