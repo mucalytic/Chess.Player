4ffbb090a07db094ef811ef8b8c4fe00b1af2ca7
Mucalytic <mucalytic@hotmail.com>
2018-03-23 17:10:13 +0000

    Update dead.ts

--- src/player/dead.ts
+++ src/player/dead.ts
@@ -4,6 +4,10 @@ import {Player} from "../player"
 export class Dead extends Player {
     name: string = "Dead";
     turn: number = 0;
+    
+    pivot(): [number, number] {
+        throw new Error("Not implemented");
+    }
 
     rotate(vector: Vector, radius: number): [number, number] {
         throw new Error("Not implemented");
