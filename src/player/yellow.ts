d09c16826bbaadcf4c894d3bf2bd98621466d0e7
Mucalytic <mucalytic@hotmail.com>
2018-03-23 17:11:02 +0000

    Update yellow.ts

--- src/player/yellow.ts
+++ src/player/yellow.ts
@@ -4,9 +4,14 @@ import {Player} from "../player"
 export class Yellow extends Player {
     name: string = "Yellow";
     turn: number = 3;
+    
+    pivot(): [number, number] {
+        return [this.piece.square.x,
+           13 - this.piece.square.y];
+    }
 
     rotate(vector: Vector, radius: number): [number, number] {
-        return [this.piece.square.n - vector.x1(radius),
-                this.piece.square.m - vector.y1(radius)];
+        return [this.piece.square.x - vector.x1(radius),
+                this.piece.square.y - vector.y1(radius)];
     }
 }
